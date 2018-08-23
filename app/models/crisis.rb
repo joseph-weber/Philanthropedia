b49f3b6410e1b1557b420cd32b666502b07d2708
Joseph Weber <joseph.theodore.weber@gmail.com>
2018-08-23 15:24:17 -0400

    fixed ports

--- app/models/crisis.rb
+++ app/models/crisis.rb
@@ -1,5 +1,10 @@
 class Crisis
-  DB = PG.connect(host: "localhost", port: 5432, dbname: 'Philanthropedia_development')
+  if(ENV['DATABASE_URL'])
+        uri = URI.parse(ENV['DATABASE_URL'])
+        DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
+    else
+        DB = PG.connect(host: "localhost", port: 5432, dbname: 'Philanthropedia_development')
+    end
 
 def self.all
   results = DB.exec(
