ddbc61f32d83c26c0933bf732ded80e4e673c302
Joseph Weber <joseph.theodore.weber@gmail.com>
2018-09-12 19:16:00 -0400

    fixed footer links

--- public/js/Footer.js
+++ public/js/Footer.js
@@ -9,8 +9,8 @@ class Footer extends React.Component {
     return(
       <div className="footer">
         <div>
-          <h1><a href="http://joseph-weber.github.io">My Github</a></h1>
-          <h1><a href="http://reddit.com/r/birdswitharms">Birds with Arms</a></h1>
+          <h1><a href="http://joseph-weber.github.io">My Portfolio</a></h1>
+          <h1><a href="https://github.com/joseph-weber">My Github</a></h1>
         </div>
         <div>
           <h1>A Joseph Weber Joint</h1>
